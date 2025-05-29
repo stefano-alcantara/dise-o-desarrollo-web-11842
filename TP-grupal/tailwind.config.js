52e81b22e6b299c9077e16110f86aed0cd1d3bbe
NalvinCode <mariano.alvarez.personal@gmail.com>
2025-05-28 21:24:16 -0300

    Ajustes en layout responsive

---
+++ TP-grupal/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        fondo: "#f5e8cc",
+        fondoHeader: "#f5e0b7",
+        fondoFooter: "#EED39D",
+        textoFooter: "#5E4C27",
+        hoverBtn: "#F8EDD7",
+        bordeBtn: "#EED39D",
+      },
+      fontFamily: {
+        baloo: ["'Baloo Paaji 2'", "Arial", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
